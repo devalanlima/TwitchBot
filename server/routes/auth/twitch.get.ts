08ee570b27fa152d76719ed59b4a49bee3b8940c
Alan Lima <dev.alanlima@gmail.com>
2024-06-20 20:50:33 -0300

    feat(auth): add Twitch OAuth route

---
+++ server/routes/auth/twitch.get.ts
@@ -0,0 +1,49 @@
+export default oauth.twitchEventHandler({
+  config:{
+    scope:[
+      'channel:bot',
+      'chat:edit',
+      'channel:moderate',
+      'user:bot',
+      'chat:read',
+      'user:write:chat',
+      'whispers:read',
+      'whispers:edit',
+      'user:read:chat',
+    ],
+    authorizationParams:{
+      force_verify: 'true',
+    }
+  },
+  async onSuccess(event, { user, tokens }) {  
+    function formatedDateTime() {
+      const now = new Date();
+      
+      const hours = String(now.getHours()).padStart(2, '0');
+      const minutes = String(now.getMinutes()).padStart(2, '0');
+      
+      const day = String(now.getDate()).padStart(2, '0');
+      const month = String(now.getMonth() + 1).padStart(2, '0');
+      const year = now.getFullYear();
+      
+      const time = `${hours}:${minutes}`;
+      const date = `${day}/${month}/${year}`;
+      
+      return `${date} - ${time}`;
+    }  
+
+    await setUserSession(event, {
+      user: {
+        id: user.id,
+        tokens: tokens,
+      },
+      //@ts-ignore
+      loggedInAt: formatedDateTime(),
+    })
+    return sendRedirect(event, '/')
+  },
+  onError(event, error) {
+    console.error('Twitch OAuth error:', error)
+    return sendRedirect(event, '/')
+  },
+})
