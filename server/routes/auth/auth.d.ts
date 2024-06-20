4d89147e42439efba1f4992fe9701ff86306596f
Alan Lima <dev.alanlima@gmail.com>
2024-06-20 20:52:54 -0300

    feat(auth): add User type declaration for user session

---
+++ server/routes/auth/auth.d.ts
@@ -0,0 +1,15 @@
+declare module '#auth-utils' {
+  interface User {
+  "id": string, 
+  "tokens": 
+    { 
+      "access_token": string, 
+      "expires_in": number, 
+      "refresh_token": string, 
+      "scope": Array<string>, 
+      "token_type": string 
+    },
+  }
+}
+
+export {}
