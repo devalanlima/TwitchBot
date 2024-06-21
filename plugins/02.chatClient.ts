9d22c3cfd6fda98c2446ad77ae71ea6d79100750
Alan Lima <dev.alanlima@gmail.com>
2024-06-20 22:09:03 -0300

    feat(plugin): create chatClient plugin with Twurple chat

---
+++ plugins/02.chatClient.ts
@@ -0,0 +1,14 @@
+import { StaticAuthProvider } from '@twurple/auth';
+import { ChatClient } from '@twurple/chat';
+
+export default defineNuxtPlugin(async (nuxtApp)=>{
+  const { $authProvider } = useNuxtApp();
+  const authProvider = $authProvider as StaticAuthProvider
+  const chatClient = new ChatClient ({ authProvider });
+
+  return {
+    provide: {
+      chatClient: chatClient,
+    }
+}
+})
