a70bb2d4dc4123a8486ce22263be150d7b925f7c
Alan Lima <dev.alanlima@gmail.com>
2024-06-23 12:07:31 -0300

    feat(composable): create useParseEmotes composable to parse and store messages with emotes

---
+++ composables/useParseEmotes.ts
@@ -0,0 +1,83 @@
+import { EmoteFetcher, EmoteParser } from "@mkody/twitch-emoticons";
+import { ApiClient } from "@twurple/api";
+import { buildEmoteImageUrl, type ParsedMessagePart } from "@twurple/chat";
+import { useGlobalStore } from "~/store/useGlobalStore";
+
+export default function useParseEmotes(channelId: number, parsedMessagePart: Array<ParsedMessagePart>) {
+
+  let text = '';
+
+  parsedMessagePart.map((response) => {
+    if (response.type === 'text') {
+      text += response.text;
+    } else if (response.type === 'emote') {
+      if (response.id) {
+        text += `<img alt="${response.name}" src="${buildEmoteImageUrl(response.id)}">`;
+      };
+    };
+  });
+
+  const { $authProvider: authProvider } = useNuxtApp();
+
+  const apiClient = authProvider && new ApiClient({ authProvider });
+
+  // @ts-ignore
+  const fetcher = new EmoteFetcher(null, null,{
+    apiClient: apiClient,
+  });  
+
+  const parser = new EmoteParser(fetcher, {
+    template: '<img alt="{name}" src="{link}">',
+    match: /(?<!alt=")(\w+)(?!")/g,
+  });
+
+  Promise.all([
+    fetcher.fetchTwitchEmotes(),
+    fetcher.fetchTwitchEmotes(channelId),
+    fetcher.fetchBTTVEmotes(),
+    fetcher.fetchBTTVEmotes(channelId),
+    fetcher.fetchSevenTVEmotes(),
+    fetcher.fetchFFZEmotes(),
+    fetcher.fetchFFZEmotes(channelId),
+    fetcher.fetchSevenTVEmotes(),
+    fetcher.fetchSevenTVEmotes(channelId, 'webp'),
+  ]).then(() => {
+    let parsedText = parser.parse(text);
+    
+    const regex = /<img alt="([^"]+)" src="([^"]+)">/g;
+    const { messages } = useGlobalStore();
+    let result: ParsedMessage = [];
+    let lastIndex = 0;
+    let match;
+
+    while ((match = regex.exec(parsedText)) !== null) {
+      if (match.index > lastIndex) {
+        result.push({
+          type: 'text',
+          value: parsedText.slice(lastIndex, match.index),
+        });
+      }
+      result.push({
+        type: 'emote',
+        value: {
+          src: match[2],
+          alt: match[1],
+        }
+      });
+      lastIndex = regex.lastIndex;
+    }
+
+    if (lastIndex < parsedText.length) {
+      result.push({
+        type: 'text',
+        value: parsedText.slice(lastIndex),
+      });
+    }
+
+    messages.push(result);
+    
+  }).catch(err => {
+    console.error('Error loading emotes...');
+    console.error(err);
+  });
+}
