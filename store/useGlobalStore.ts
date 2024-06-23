73d37e33b7a39049dcc85ea8816fb155da7ee8b4
Alan Lima <dev.alanlima@gmail.com>
2024-06-23 19:21:32 -0300

    refactor(store): update messages type in global store to MessageData

--- store/useGlobalStore.ts
+++ store/useGlobalStore.ts
@@ -1,7 +1,7 @@
 import type { ParsedMessagePart } from "@twurple/chat";
 
 export const useGlobalStore = defineStore('global', () => {
-  const messages = ref<Array<ParsedMessage>>([]);
+  const messages = ref<Array<MessageData>>([]);
 
   return { messages };
 })
