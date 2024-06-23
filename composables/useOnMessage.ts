9c539344a63583da9417a53144fb519978f56c34
Alan Lima <dev.alanlima@gmail.com>
2024-06-23 12:09:08 -0300

    refactor(composable): update useOnMessage to utilize useParseEmotes for message parsing

--- composables/useOnMessage.ts
+++ composables/useOnMessage.ts
@@ -1,5 +1,5 @@
 import { parseChatMessage, type ChatClient, type ChatMessage } from "@twurple/chat";
-import { useGlobalStore } from "~/store/useGlobalStore";
+import useParseEmotes from "./useParseEmotes";
 
 export default function useOnMessage (chatClient: ChatClient, commands: Array<ChatCommand>) {
   let chatListener;
@@ -7,51 +7,51 @@ export default function useOnMessage (chatClient: ChatClient, commands: Array<Ch
 
   const composeMessageToFilter = (message: string) => {
     return message.trim().normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().split(" ").join(""); 
-  }
+  };
 
   const keywordsExistsInText = (keyWords: KeywordPair, text: string) => {
 
     const testExactMatch = (keyword: string, text: string) => {
       const regex = new RegExp(`(^|\\s)${keyword}(\\s|$)`);
       return regex.test(text);
-    }
+    };
 
     const firstKeywordSetMatch = 
     keyWords.firstKeywordSet.some(
-      (keyWord) => keyWord.isExact ? 
-      testExactMatch(keyWord.keyword, text) : 
-      composeMessageToFilter(text).includes(composeMessageToFilter(keyWord.keyword))
+      (keyWord) => keyWord.isExact 
+      ? testExactMatch(keyWord.keyword.toLowerCase(), text.toLowerCase())
+      : composeMessageToFilter(text).includes(composeMessageToFilter(keyWord.keyword))
     );
 
     const secondKeywordSetMach = 
     keyWords.secondKeywordSet?.some(
-      (keyWord) => keyWord.isExact ? 
-      testExactMatch(keyWord.keyword, text) : 
-      composeMessageToFilter(text).includes(composeMessageToFilter(keyWord.keyword))
+      (keyWord) => keyWord.isExact
+      ? testExactMatch(keyWord.keyword.toLowerCase(), text.toLowerCase())
+      : composeMessageToFilter(text).includes(composeMessageToFilter(keyWord.keyword))
     );
 
     if (keyWords.secondKeywordSet) {
       return firstKeywordSetMatch ? secondKeywordSetMach : false;
     } else {
       return firstKeywordSetMatch;
-    }
+    };
 
-  }
+  };
 
   const pickRandom = (list: Array<any>) => {
     return list[~~(Math.random() * list.length)];
-  }
+  };
 
-  const globalStore = useGlobalStore();
+  chatListener = chatClient.onMessage(async (channel: string, user: string, text: string, msg: ChatMessage)=>{
+    useParseEmotes(Number(msg.channelId), parseChatMessage(text, msg.emoteOffsets));
 
-  chatListener = chatClient.onMessage((channel: string, user: string, text: string, msg: ChatMessage)=>{
-    globalStore.messages?.push(parseChatMessage(text, msg.emoteOffsets));
-    
     commands.map((command): void => {
       if (keywordsExistsInText(command.keywordSet, text)) {
-        chatClient.say(channel, pickRandom(command.responses));
+        const response = pickRandom(command.responses);
+        chatClient.say(channel, response);
+        useParseEmotes(Number(msg.channelId), parseChatMessage(response, msg.emoteOffsets));
       }
-    })
-  })
+    });
 
-}
+  });
+};
