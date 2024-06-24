33806eeb13a26d077637a4e7f3db65d602bfafd2
Alan Lima <dev.alanlima@gmail.com>
2024-06-24 18:58:28 -0300

    feat(composable): update useStoreMessage to include ID and time properties and time function

--- composables/useStoreMessage.ts
+++ composables/useStoreMessage.ts
@@ -6,11 +6,19 @@ export default async function useStoreMessage (message: string, msg: ChatMessage
 
   const parsedMessage = await useParseChatMessage(Number(msg.channelId), parseChatMessage(message, msg.emoteOffsets));  
   const finalUser = useBuildCustomUser(msg.userInfo);
+
+  const time = () => {
+    const hour = new Date().getHours().toString().padStart(2, '0');
+    const min = new Date().getMinutes().toString().padStart(2, '0');
+    return `${hour}:${min}`;
+  };
   
   if (parsedMessage) {
     messages.push({
+      id: msg.id,
       user: finalUser,
       message: parsedMessage,
+      time: time(),
     });
   };
 };
