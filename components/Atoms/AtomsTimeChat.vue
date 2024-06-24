b5d9ff60fa1f698f1e8c774ec46cdd99c140ea68
Alan Lima <dev.alanlima@gmail.com>
2024-06-24 19:03:59 -0300

    feat(component): create component to show message timestamp

---
+++ components/Atoms/AtomsTimeChat.vue
@@ -0,0 +1,12 @@
+<template>
+  <span class="font-Inter mr-[2px] font-thin opacity-50">{{ props.time }}</span>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  time: string
+}
+
+const props = defineProps<Props>();
+
+</script>
