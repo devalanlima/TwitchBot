52153d36fdc7eb1f496d290fd432968953a0f645
Alan Lima <dev.alanlima@gmail.com>
2024-06-24 19:06:29 -0300

    feat(component): create component for rendering chat messages with emotes

---
+++ components/Atoms/AtomsChatMessage.vue
@@ -0,0 +1,24 @@
+<template>
+  <template v-for="message in props.message">
+    <span v-if="message.type === 'text'" class="break-words">
+      {{ message.value }}
+    </span>
+    <span v-if="message.type === 'emote'" class="relative inline-flex items-center h-[13px] mx-[2px] w-[24px]">
+      <img 
+      v-tooltip.top="message.value.alt"
+      class="absolute min-w-fit min-h-fit"
+      :src="message.value.src" 
+      :alt="message.value.alt"
+      />
+    </span>
+  </template>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  message: ParsedMessage;
+}
+
+const props = defineProps<Props>();
+
+</script>
