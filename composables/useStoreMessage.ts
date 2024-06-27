f5c0020bfdb5b0394e1e07b768277b6a4dd9edb5
Alan Lima <dev.alanlima@gmail.com>
2024-06-27 17:10:27 -0300

    refactor(useStoreMessage): adapt message storage logic to include channels

--- composables/useStoreMessage.ts
+++ composables/useStoreMessage.ts
@@ -1,7 +1,7 @@
-import { parseChatMessage, type ChatMessage } from "@twurple/chat";
+import { parseChatMessage, toChannelName, toUserName, type ChatMessage } from "@twurple/chat";
 import { useGlobalStore } from "~/store/useGlobalStore";
 
-export default async function useStoreMessage (message: string, msg: ChatMessage) {
+export default async function useStoreMessage (message: string, msg: ChatMessage, channel: string) {
   const { messages } = useGlobalStore();
 
   const parsedMessage = await useParseChatMessage(Number(msg.channelId), parseChatMessage(message, msg.emoteOffsets));  
@@ -12,13 +12,24 @@ export default async function useStoreMessage (message: string, msg: ChatMessage
     const min = new Date().getMinutes().toString().padStart(2, '0');
     return `${hour}:${min}`;
   };
-  
+
+  const channelKey = `#${channel}`
+      
   if (parsedMessage) {
-    messages.push({
+
+    const found = messages.find(channel => channel.hasOwnProperty(channelKey));
+    const atualMessage = {
       id: msg.id,
       user: finalUser,
       message: parsedMessage,
       time: time(),
-    });
+    }
+
+    if (found) {
+      messages[messages.findIndex(channel => channel.hasOwnProperty(channelKey))][channelKey].push(atualMessage) 
+      if (messages[messages.findIndex(channel => channel.hasOwnProperty(channelKey))][channelKey].length > 30) {
+        messages[messages.findIndex(channel => channel.hasOwnProperty(channelKey))][channelKey].shift();
+      }
+    }
   };
 };
