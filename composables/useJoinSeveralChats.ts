460464524b5c44f777b55acfb24aeffe60c3551a
Alan Lima <dev.alanlima@gmail.com>
2024-06-27 16:57:07 -0300

    refactor(composable): update useJoinSeveralChats to save channels in globalStore and cookies

--- composables/useJoinSeveralChats.ts
+++ composables/useJoinSeveralChats.ts
@@ -1,12 +1,41 @@
 import type { ChatClient } from "@twurple/chat";
+import { useGlobalStore } from "~/store/useGlobalStore";
 
-export default function useJoinSeveralChats (chatClient: ChatClient, channels: Array<string>): void {
+export default async function useJoinSeveralChats (chatClient: ChatClient, channelsToJoin: Array<string>, joinMessage?: string) {
+  const loading = ref(true);
+
+  const globalStore = useGlobalStore();
+  const channelCookies = useCookie('channelCookies');
   
-  channels.map((channel) => {
-    if (!chatClient.currentChannels.includes(channel)) {
-      chatClient.join(channel);
-      chatClient.say(channel, 'farenaBot entrou.')
-    }
-  });
+  if (globalStore.messages.length > 3) {
+    loading.value = false;
+    alert('Máximo de canais simultaneos atingido');
+  } else {
 
+    if (!chatClient.isConnected) {
+      chatClient.connect();
+      useOnMessage(chatClient);
+    } else {
+      console.log('already connected');
+    };
+
+    channelsToJoin.map(async (channel) => {
+      if (!chatClient.currentChannels.includes(channel)) {
+        await chatClient.join(channel).then(()=>{
+          globalStore.currentChannels = chatClient.currentChannels;
+          channelCookies.value = JSON.stringify(globalStore.currentChannels);
+          globalStore.messages.push({
+            [channel]: [],
+          });
+        });     
+
+        joinMessage && chatClient.say(channel, joinMessage);      
+        loading.value = false;
+      } else {
+        console.log(`already connected at ${channel}`);
+      };
+    });
+  }
+
+  return loading;
 }
