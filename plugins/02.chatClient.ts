4820739a73d9ba74b17d486078bb11d7bf52b43d
Alan Lima <dev.alanlima@gmail.com>
2024-07-08 12:34:14 -0300

    style: Code has been formatted

--- plugins/02.chatClient.ts
+++ plugins/02.chatClient.ts
@@ -3,12 +3,12 @@ import { ChatClient } from '@twurple/chat';
 
 export default defineNuxtPlugin(async (nuxtApp)=>{
   const { $authProvider } = useNuxtApp();
-  const authProvider = $authProvider as StaticAuthProvider
+  const authProvider = $authProvider as StaticAuthProvider;
   const chatClient = new ChatClient ({ authProvider });
 
   return {
     provide: {
       chatClient: chatClient,
-    }
-}
-})
+    },
+  };
+});
