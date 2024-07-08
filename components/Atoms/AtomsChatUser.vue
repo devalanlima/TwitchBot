082012409478cc68cfe782cda508a4ebe6b9f0f7
Alan Lima <dev.alanlima@gmail.com>
2024-07-08 11:18:49 -0300

    Feat(component): Add Bot badge in badgePaths

--- components/Atoms/AtomsChatUser.vue
+++ components/Atoms/AtomsChatUser.vue
@@ -18,7 +18,7 @@
       </template>
     </span>
 
-    <span :style="`color: ${user.color}`" class="font-semibold">
+    <span :style="`color: ${user.color}`" class="font-semibold self-start">
       {{ user.displayName }}:
     </span>
   </span>
@@ -30,40 +30,43 @@ interface Props {
 };
 
 const props = defineProps<Props>();
-const badges = props.customUser.badges;
-const user = props.customUser.username;
+const user = props.customUser;
 
 const badgePaths = [
   {
     alt: 'Broadcaster',
-    exist: badges.isBroadcaster,
+    exist: user.isBroadcaster,
     url: 'https://static-cdn.jtvnw.net/badges/v1/5527c58c-fb7d-422d-b71b-f309dcb85cc1/1',
   },
   {
     alt: 'Moderator',
-    exist: badges.isModerator,
+    exist: user.isMod,
     url: 'https://static-cdn.jtvnw.net/badges/v1/3267646d-33f0-4b17-b3df-f923a41db1d0/1',
   },
   {
     alt: 'Subscriber',
-    exist: badges.isSubscriber,
+    exist: user.isSubscriber,
     url: 'https://static-cdn.jtvnw.net/badges/v1/5d9f2208-5dd8-11e7-8513-2ff4adfae661/1',
   },
   {
     alt: 'Vip',
-    exist: badges.isVip,
+    exist: user.isVip,
     url: 'https://static-cdn.jtvnw.net/badges/v1/b817aba4-fad8-49e2-b88a-7cc744dfa6ec/1',
   },
   {
     alt: 'Founder',
-    exist: badges.isFounder,
+    exist: user.isFounder,
     url: 'https://static-cdn.jtvnw.net/badges/v1/511b78a9-ab37-472f-9569-457753bbe7d3/1',
   },
   {
     alt: 'Artist',
-    isArtist: badges.isArtist,
+    exist: user.isArtist,
     url: 'https://assets.help.twitch.tv/article/img/000002399-05.png',
   },
-]
-
+  {
+    alt: "Bot",
+    exist: user.isBot,
+    url: '/botBadge.png'
+  },
+];
 </script>
