a8d4d2d3876fa967e7f8e1530efaa1d9e005673e
Alan Lima <dev.alanlima@gmail.com>
2024-07-08 12:19:41 -0300

    refactor(composable): Now leaves properly and remove channel from cookies and remove emotes from localstorage

--- composables/useLeaveChat.ts
+++ composables/useLeaveChat.ts
@@ -1,19 +1,26 @@
+import { ApiClient } from "@twurple/api";
+import type { StaticAuthProvider } from "@twurple/auth";
 import type { ChatClient } from "@twurple/chat";
 import { useGlobalStore } from "~/store/useGlobalStore";
 
-export default function useLeaveChat (chatClient: ChatClient, channel: string) {
+export default async function useLeaveChat (chatClient: ChatClient, channel: string) {
   const globalStore = useGlobalStore();
+  const { $authProvider } = useNuxtApp();
+  const authProvider = $authProvider as StaticAuthProvider;
+  const api = authProvider && new ApiClient({ authProvider });
 
   if (globalStore.currentChannels.includes(channel)) {
     chatClient.part(channel);
     globalStore.currentChannels = globalStore.currentChannels.filter(currentChannel => channel !== currentChannel); 
     
-    globalStore.messages = globalStore.messages.filter(obj => {
-      return !Object.keys(obj).includes(channel);
-    });
+    delete globalStore.chatMessages[channel.slice(1)];
+    
+    await api?.users.getUserByName(channel.slice(1)).then((user)=>{
+      user?.id && localStorage.removeItem(user?.id);
+    })
 
   } else {
-    console.log('channel is not connected');
+    console.log('Channel is not connected');
   }
 
   const channelCookies = useCookie('channelCookies');  
