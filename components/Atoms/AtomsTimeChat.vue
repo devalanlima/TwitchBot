05564a37350089fcbfed7fb97a8f7155964d4f77
Alan Lima <dev.alanlima@gmail.com>
2024-07-08 11:22:07 -0300

    refactor(component): Now component receive Date type and formats it internally

--- components/Atoms/AtomsTimeChat.vue
+++ components/Atoms/AtomsTimeChat.vue
@@ -1,12 +1,17 @@
 <template>
-  <span class="font-Inter mr-[2px] font-thin opacity-50">{{ props.time }}</span>
+  <span class="font-Inter mr-[4px] font-thin opacity-50">{{ time }}</span>
 </template>
 
 <script setup lang="ts">
 interface Props {
-  time: string
+  time: Date
 }
 
 const props = defineProps<Props>();
+
+const hour = props.time.getHours().toString().padStart(2, '0');
+const min = props.time.getMinutes().toString().padStart(2, '0');
+
+const time = `${hour}:${min}`
 
 </script>
