0b70f3d230068929351c5c39e9334f91135f50ae
Alan Lima <dev.alanlima@gmail.com>
2024-07-08 12:31:10 -0300

    refactor(composable): Now only stores 30 messages and doesn't build them

--- composables/useStoreMessage.ts
+++ composables/useStoreMessage.ts
@@ -1,35 +1,18 @@
-import { parseChatMessage, toChannelName, toUserName, type ChatMessage } from "@twurple/chat";
 import { useGlobalStore } from "~/store/useGlobalStore";
 
-export default async function useStoreMessage (message: string, msg: ChatMessage, channel: string) {
-  const { messages } = useGlobalStore();
+export default async function useStoreMessage (channelFrom: string, message: MessageData) {
+  const globalStore = useGlobalStore();
 
-  const parsedMessage = await useParseChatMessage(Number(msg.channelId), parseChatMessage(message, msg.emoteOffsets));  
-  const finalUser = useBuildCustomUser(msg.userInfo);
-
-  const time = () => {
-    const hour = new Date().getHours().toString().padStart(2, '0');
-    const min = new Date().getMinutes().toString().padStart(2, '0');
-    return `${hour}:${min}`;
-  };
-
-  const channelKey = `#${channel}`
-      
-  if (parsedMessage) {
-
-    const found = messages.find(channel => channel.hasOwnProperty(channelKey));
-    const atualMessage = {
-      id: msg.id,
-      user: finalUser,
-      message: parsedMessage,
-      time: time(),
-    }
-
-    if (found) {
-      messages[messages.findIndex(channel => channel.hasOwnProperty(channelKey))][channelKey].push(atualMessage) 
-      if (messages[messages.findIndex(channel => channel.hasOwnProperty(channelKey))][channelKey].length > 30) {
-        messages[messages.findIndex(channel => channel.hasOwnProperty(channelKey))][channelKey].shift();
-      }
-    }
+  const channel = globalStore.chatMessages[channelFrom];
+  
+  if (globalStore.chatMessages.hasOwnProperty(channelFrom)) {
+    channel.push(message);
+    
+    channel.length >= 30 && channel.shift();
+  } else {
+    globalStore.chatMessages = {
+      ...globalStore.chatMessages,
+      [channelFrom]: [message],
+    };
   };
 };
