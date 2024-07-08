9570b4fb2d845d844fa47888bcee8ca4ea13a144
Alan Lima <dev.alanlima@gmail.com>
2024-07-08 11:23:28 -0300

    feat(component): Add style for user bot

--- components/Molecules/MoleculesChatMessages.vue
+++ components/Molecules/MoleculesChatMessages.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-    <ul class="max-w-[334px] flex flex-col leading-[20px]">
+    <ul class="max-w-[367px] flex flex-col leading-[20px]">
       <template v-for="message in props.messages" :key="message.id">
-        <li :id="message.id" class="py-1 px-5 font-Inter text-[13px]" :style="`background-color: ${message.user.username.color}1A`" >
+        <li 
+        :id="message.id" 
+        class="py-1 px-5 font-Inter text-[14px]" 
+        :class="message.user.isBot && 'border-r-2 border-white'"
+        :style="`background-color: ${message.user.color}12`" 
+        >
           <AtomsTimeChat
             :time="message.time"
           />
-          <AtomsChatUser
+          <AtomsChatUser            
             :custom-user="message.user"
           />
           <AtomsChatMessage
-            :message="message.message"
+            :chat-message="message.data"
           />
         </li>
       </template>
