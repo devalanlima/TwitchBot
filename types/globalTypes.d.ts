9b09c9ed04f6c237baedcebf95388ef82736e6e5
Alan Lima <dev.alanlima@gmail.com>
2024-08-05 12:12:58 -0300

    feat(types): create CommandType and TimeSet interfaces

--- types/globalTypes.d.ts
+++ types/globalTypes.d.ts
@@ -3,70 +3,78 @@ import type { ChatMessage, ChatUser } from "@twurple/chat";
 declare global {
 
   interface Keyword {
-    keyword: string;
-    isExact: boolean;
+    keyword: string
+    isExact: boolean
   }
 
   interface KeywordPair {
-    firstKeywordSet: Array<Keyword>;
-    secondKeywordSet?: Array<Keyword>;
+    firstKeywordSet: Array<Keyword>
+    secondKeywordSet?: Array<Keyword>
   }
 
   interface CommandResponse {
-    id: number, 
+    id: number
     response: string
   }
 
+  type CommandType = 'keyword' | 'timer'
+
+  interface TimeSet {
+    mode: CommandType
+    time: number
+  }
+
   interface ChatCommand {
-    name: string,
-    keywordSet: KeywordPair;
-    responses: Array<CommandResponse>;
+    name: string
+    timeSet: TimeSet
+    keywordSet: KeywordPair
+    responses: Array<CommandResponse>
   }
 
   interface ParsedText {
-    type: 'text',
-    value: string,
+    type: 'text'
+    value: string
   }
 
   interface ParsedEmote {
-    type: 'emote',
+    type: 'emote'
     value: {
-      src: string,
-      alt: string,
+      src: string
+      alt: string
     }
   }
 
   type ParsedMessage = Array<ParsedText | ParsedEmote>
 
   interface CustomUser {
-    displayName: string,
-    color: string | undefined,
-    isBot: boolean,
-    isBroadcaster: boolean,
-    isMod: boolean,
-    isSubscriber: boolean,
-    isVip: boolean,
-    isFounder: boolean,
-    isArtist: boolean,
+    displayName: string
+    color: string | undefined
+    isBot: boolean
+    isBroadcaster: boolean
+    isMod: boolean
+    isSubscriber: boolean
+    isVip: boolean
+    isFounder: boolean
+    isArtist: boolean
   }
 
   interface MessageData {
-    id: string,
-    time: Date,
-    user: CustomUser,
-    data: ParsedMessage,
+    id: string
+    time: Date
+    user: CustomUser
+    data: ParsedMessage
   }
 
   interface ChatData {
-    [key: string]: Array<MessageData>,
+    [key: string]: Array<MessageData>
   }
 
   interface EmotesResponse {
-    "provider": 0 | 1 | 2 | 3,
-    "code": string,
+    "provider": 0 | 1 | 2 | 3
+    "code": string
     "urls": [
       {
-        "size": '1x' | '2x' | '3x' | '4x',
+        "size": '1x' | '2x' | '3x' | '4x'
         "url": string
       }
     ]
