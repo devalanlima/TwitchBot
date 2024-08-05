f6eb22dc3368757594c75d71c4bbf4c35a75a02e
Alan Lima <dev.alanlima@gmail.com>
2024-08-05 12:16:04 -0300

    feat(component): Create Save and Discard buttons component

---
+++ components/Molecules/MoleculesSaveDiscard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex gap-3">
+    <Button
+    class="max-w-fit self-end"
+    @click="emit('discard')"
+    label="Discard"
+    severity="danger"
+    icon="pi pi-times"
+    icon-pos="right"
+    :disabled="props.discardDisabled"
+    />
+    <Button
+    class="max-w-fit self-end"
+    @click="emit('save')"
+    label="Save"
+    severity="success"
+    :disabled="props.saveDisabled"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+  saveDisabled?: boolean
+  discardDisabled?: boolean
+}
+
+const props = defineProps<Props>();
+const emit = defineEmits(['save', 'discard'])
+
+</script>
